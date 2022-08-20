32010970665106313bb51ba507ed5ef5edb122d8
levimendyk <levi3743@gmail.com>
2022-08-20 15:36:33 -0600

    Added some code to the Javascript for initial setup

--- index.js
+++ index.js
@@ -2,8 +2,36 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is the project title?",
+  "What is the description?",
+  "What is the installation instructions?",
+  "What is the usage informations?",
+  "What are the contribution guidelines?",
+  "What are the tests?",
+];
+
+// --Create an array of license options with corresponding badge?? -LEVI
+const licenses = [];
+
+// --Create an array of Questions (github profile and email address) -LEVI
+const questionsTwo = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
